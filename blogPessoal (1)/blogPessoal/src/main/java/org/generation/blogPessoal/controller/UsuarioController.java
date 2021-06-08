d22503dd41020ccd90bfa29f7d62a2417af6b34e
Jonathan <jonathan.ribeiro28121997@gmail.com>
2021-06-08 15:50:37 -0300

    Atualizando controler de Usuario e BasicSecurityConfig

--- blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
+++ blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -2,13 +2,18 @@ package org.generation.blogPessoal.controller;
 
 import java.util.Optional;
 
+import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.model.UserLogin;
 import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.PostagemRepository;
+import org.generation.blogPessoal.repository.UsuarioRepository;
 import org.generation.blogPessoal.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +24,9 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UsuarioController {
 
+	@Autowired  // INJETAR DE DEPENDENCIA, ASSIM ESSE REPOSITORIO É ACESSADO
+	private UsuarioRepository repository;
+	
 	
 	@Autowired
 	private UsuarioService usuarioService;
@@ -27,6 +35,11 @@ public class UsuarioController {
 	public ResponseEntity<UserLogin> Autentication(@RequestBody Optional<UserLogin> user){
 		return usuarioService.Logar(user).map(resp -> ResponseEntity.ok(resp))
 				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> GetById(@PathVariable Long id){
+	return repository.findById(id).map(resp -> ResponseEntity.ok(resp)).orElse(ResponseEntity.notFound().build());
 	}
 	
 	@PostMapping("/cadastrar")
@@ -38,5 +51,4 @@ public class UsuarioController {
 			return ResponseEntity.badRequest().build(); 
 		}
 	}
- 	
 }
--- blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal (1)/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -14,73 +14,79 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@Entity  // Determina entidade do JPA 
-@Table(name = "postagem")  // Essa entidade virara uma tabela de nome "postagem"
+@Entity // Determina entidade do JPA
+@Table(name = "postagem") // Essa entidade virara uma tabela de nome "postagem"
 public class Postagem {
-	
-	@Id   
-	@GeneratedValue(strategy = GenerationType.IDENTITY)  // VALOR QUE VAI SER GERADO COMO CHAVE ESTRANGEIRA DA CHAVE ID
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // VALOR QUE VAI SER GERADO COMO CHAVE ESTRANGEIRA DA CHAVE ID
 	private long id;
-	
-	@NotNull  // NÃO PODE SER VAZIO 
-	@Size(min = 5, max = 100)  // MINIMO DE CARACTER E MAXIMO
+
+	@NotNull // NÃO PODE SER VAZIO
+	@Size(min = 5, max = 100) // MINIMO DE CARACTER E MAXIMO
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
-	
-	@Temporal(TemporalType.TIMESTAMP)  // TEMPORAL DETERMINA A HORA QUE A POSTAGEM PASSOU EXATAMENTE 
+
+	@Temporal(TemporalType.TIMESTAMP) // TEMPORAL DETERMINA A HORA QUE A POSTAGEM PASSOU EXATAMENTE
 	private Date data = new java.sql.Date(System.currentTimeMillis());
-	
+
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
-	
+
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Usuario usuario;
-	
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
 	public String getTitulo() {
 		return titulo;
 	}
+
 	public void setTitulo(String titulo) {
 		this.titulo = titulo;
 	}
+
 	public String getTexto() {
 		return texto;
 	}
+
 	public void setTexto(String texto) {
 		this.texto = texto;
 	}
+
 	public Date getData() {
 		return data;
 	}
+
 	public void setData(Date data) {
 		this.data = data;
 	}
-	
+
 	public Tema getTema() {
 		return tema;
 	}
+
 	public void setTema(Tema tema) {
 		this.tema = tema;
 	}
+
 	public Usuario getUsuario() {
 		return usuario;
 	}
+
 	public void setUsuario(Usuario usuario) {
 		this.usuario = usuario;
 	}
-	
-	
-	
 
 }
